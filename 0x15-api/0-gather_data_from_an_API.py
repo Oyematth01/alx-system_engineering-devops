f50f44a8668ea55795d61eb296ba1f14e3827c07
Oyematth01 <oyematth@gmail.com>
2024-07-30 08:59:03 +0100

    task0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,60 +1,29 @@
 #!/usr/bin/python3
-"""
-This script fetches and displays TODO list progress for a given employee ID.
-"""
+'''A script that gathers employee name,tasks,total number of tasks from an API
+'''
+
+import re
 import requests
 import sys
 
+REST_API = "https://jsonplaceholder.typicode.com"
 
-def get_employee_todo_progress(employee_id):
-    """
-    Fetch and display the TODO list progress for a given employee ID.
-    """
-    try:
-        # Fetch employee details
-        user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-        user_response = requests.get(user_url)
-        user_data = user_response.json()
-
-        if not user_data:
-            print(f"User with ID {employee_id} not found.")
-            return
-
-        employee_name = user_data.get('name')
-
-        # Fetch TODO list for the employee
-        todos_url = (
-            f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-        )
-        todos_response = requests.get(todos_url)
-        todos_data = todos_response.json()
-
-        total_tasks = len(todos_data)
-        completed_tasks = [
-            task for task in todos_data if task.get('completed')
-        ]
-        num_completed_tasks = len(completed_tasks)
-
-        # Display the result
-        print(
-            f"Employee {employee_name} is done with tasks("
-            f"{num_completed_tasks}/{total_tasks}):"
-        )
-        for task in completed_tasks:
-            print(f"\t {task.get('title')}")
-
-    except Exception as e:
-        print(f"Error: {e}")
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./script.py <employee_id>")
-        sys.exit(1)
-
-    try:
-        employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
-    except ValueError:
-        print("Employee ID must be an integer")
-        sys.exit(1)
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            emp_req = requests.get('{}/users/{}'.format(REST_API, id)).json()
+            task_req = requests.get('{}/todos'.format(REST_API)).json()
+            emp_name = emp_req.get('name')
+            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
+            comp_tasks = list(filter(lambda x: x.get('completed'), tasks))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    emp_name,
+                    len(comp_tasks),
+                    len(tasks)
+                )
+            )
+            if len(comp_tasks) > 0:
+                for task in comp_tasks:
+                    print('\t {}'.format(task.get('title')))
